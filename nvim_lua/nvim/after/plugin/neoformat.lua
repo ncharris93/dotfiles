e4b7f2df0eef4b9df4ceee00f93b094f190ea9f3
Noah Harris <ncharris93@gmail.com>
2023-04-06 15:59:42 -0400

    add rn config

--- nvim_lua/nvim/after/plugin/neoformat.lua
+++ nvim_lua/nvim/after/plugin/neoformat.lua
@@ -1,13 +1,21 @@
 vim.cmd([[
    let g:neoformat_basic_format_align = 1
+   let g:neoformat_try_node_exe = 1
+   "let g:neoformat_verbose = 1
+
+   let g:neoformat_typescript_nchpretttier = {
+      \ 'args':['--write','--config .prettierrc'],
+      \ 'replace':1
+   }
+
+   let g:neoformat_enabled_typescrpt = ['nchprettier']
+
 
    augroup fmt
       autocmd!
       autocmd BufWritePre * undojoin | Neoformat
    augroup END
 ]])
-
-
 
 vim.keymap.set("n", "<leader>f", vim.cmd.Neoformat)
 
--- zsh/.zprofile
+++ zsh/.zprofile
@@ -4,3 +4,8 @@ eval "$(/opt/homebrew/bin/brew shellenv)"
 export PYENV_ROOT="$HOME/.pyenv"
 export PATH="$PYENV_ROOT/bin:$PATH"
 eval "$(pyenv init --path)"
+
+# Android Studio ReactNative
+export ANDROID_HOME=$HOME/Library/Android/sdk
+export PATH=$PATH:$ANDROID_HOME/emulator
+export PATH=$PATH:$ANDROID_HOME/platform-tools
