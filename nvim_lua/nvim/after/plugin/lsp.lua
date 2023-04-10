3fc8d62f7dbdf5f73401899142f5e6f86ca97405
Noah Harris <ncharris93@gmail.com>
2023-04-10 18:03:10 -0400

    fix sumneko_lua errors

--- nvim_lua/nvim/after/plugin/lsp.lua
+++ nvim_lua/nvim/after/plugin/lsp.lua
@@ -5,12 +5,11 @@ lsp.preset('recommended')
 lsp.ensure_installed({
 	'tsserver',
 	'eslint',
-	'sumneko_lua',
 	'rust_analyzer'
 })
 
 -- Fix Undefined global 'vim'
-lsp.configure('sumneko_lua', {
+lsp.configure('lua_ls', {
     settings = {
         Lua = {
             diagnostics = {
--- nvim_lua/nvim/after/plugin/nerdtree.lua
+++ nvim_lua/nvim/after/plugin/nerdtree.lua
@@ -1,2 +1,6 @@
 vim.keymap.set("n", "<C-t>", vim.cmd.NERDTreeToggle);
 vim.keymap.set("n", "<C-f>", vim.cmd.NERDTreeFind);
+
+vim.cmd([[
+   let g:NERDTreeShowHidden=1
+]])
