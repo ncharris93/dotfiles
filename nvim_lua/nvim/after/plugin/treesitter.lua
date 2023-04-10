57001383b6e32d9155ba0834191ef9b954b3998d
Noah Harris <ncharris93@gmail.com>
2023-04-10 18:03:17 -0400

    add dotfiles to treesitter view

--- nvim_lua/nvim/after/plugin/treesitter.lua
+++ nvim_lua/nvim/after/plugin/treesitter.lua
@@ -1,6 +1,6 @@
 require'nvim-treesitter.configs'.setup {
   -- A list of parser names, or "all"
-  ensure_installed = { "help", "javascript", "typescript", "c", "lua", "rust", "markdown", "vim" },
+  ensure_installed = {"javascript", "typescript", "c", "lua", "rust", "markdown", "vim" },
 
   -- Install parsers synchronously (only applied to `ensure_installed`)
   sync_install = true,
