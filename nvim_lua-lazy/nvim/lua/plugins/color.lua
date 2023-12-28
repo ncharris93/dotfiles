d8d578256b5abe8e1b77246dff331c6aef4d39a8
Noah Harris <ncharris93@gmail.com>
2023-12-27 20:01:31 -0500

    chore: cleanup color

--- nvim_lua-lazy/nvim/lua/plugins/color.lua
+++ nvim_lua-lazy/nvim/lua/plugins/color.lua
@@ -6,6 +6,10 @@ return {
     opts = {
       transparent = true
     },
+    -- allows setting for theme without importing LazyVim defaults?
+    -- config = function()
+    --   vim.cmd([[colorscheme tokyonight-night]])
+    -- end
   },
   {
     "LazyVim/LazyVim",
@@ -14,43 +18,4 @@ return {
     },
   }
 }
--- return {
---   {
---     "ellisonleao/gruvbox.nvim",
---     config = function()
---       require("gruvbox").setup({
---         terminal_colors = true, -- add neovim terminal colors
---         undercurl = true,
---         underline = true,
---         bold = true,
---         italic = {
---           strings = true,
---           emphasis = true,
---           comments = true,
---           operators = false,
---           folds = true,
---         },
---         strikethrough = true,
---         invert_selection = false,
---         invert_signs = false,
---         invert_tabline = false,
---         invert_intend_guides = false,
---         inverse = true, -- invert background for search, diffs, statuslines and errors
---         contrast = "", -- can be "hard", "soft" or empty string
---         palette_overrides = {},
---         overrides = {},
---         dim_inactive = false,
---         transparent_mode = true,
---       })
---     end,
---     priority = 1000,
---   },
---
---   {
---     "LazyVim/LazyVim",
---     opts = {
---       colorscheme = "gruvbox",
---     },
---   }
--- }
 
