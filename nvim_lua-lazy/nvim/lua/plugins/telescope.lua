af5320eca414e259ab4f57608e38e225b0a8a158
Noah Harris <ncharris93@gmail.com>
2023-12-29 17:59:44 -0500

    chore: add descriptions to telescope

--- nvim_lua-lazy/nvim/lua/plugins/telescope.lua
+++ nvim_lua-lazy/nvim/lua/plugins/telescope.lua
@@ -3,8 +3,9 @@ return {
     tag = '0.1.5',
     dependencies = { 'nvim-lua/plenary.nvim' },
     keys={
-      { "<leader>ff", "<cmd>Telescope find_files<cr>"},
-      { "<leader>fg", "<cmd>Telescope live_grep<cr>"},
+      { "<leader>ff", "<cmd>Telescope find_files<cr>", desc = "find files"},
+      { "<leader>fgh", "<cmd>Telescope find_files hidden=true<cr>", desc = "find hidden"},
+      { "<leader>fg", "<cmd>Telescope live_grep<cr>", desc = "grep"},
       { "<leader>fb", "<cmd>Telescope buffers<cr>"},
       { "<leader>fh", "<cmd>Telescope help_tags<cr>"},
       { "<leader><C-p>", "<cmd>Telescope git_files<cr>"}
