dd45a7e1c026923d054a634eaaa414ef9ff74f0a
Noah Harris <ncharris93@gmail.com>
2023-12-31 11:22:41 -0500

    fix: working copilot

--- nvim_lua-lazy/nvim/lua/plugins/copilot.lua
+++ nvim_lua-lazy/nvim/lua/plugins/copilot.lua
@@ -1,5 +1,12 @@
 return {
-    {
-        "github/copilot.vim"
-    }
+	{
+		"github/copilot.vim",
+		config = function()
+			require("copilot").setup({})
+		end,
+		init = function()
+			vim.g.copilot_no_tab_map = true
+			vim.api.nvim_set_keymap("i", "<C-J>", 'copilot#Accept("<CR>")', { silent = true, expr = true })
+		end,
+	},
 }
