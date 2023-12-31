f927bea2c0590388786d97319cd41e223dd3e4ab
Noah Harris <ncharris93@gmail.com>
2023-12-31 09:59:42 -0500

    fix: use fzf with telescope

--- nvim_lua-lazy/nvim/lua/plugins/nerd-tree.lua
+++ nvim_lua-lazy/nvim/lua/plugins/nerd-tree.lua
@@ -1,36 +1,35 @@
 return {
-  {
-    "preservim/nerdtree",
-    keys = {
-      { "<C-n>","<cmd>NERDTree<CR>"},
-      { "<C-t>","<cmd>NERDTreeToggle<CR>"},
-      { "<C-f>","<cmd>NERDTreeFind<CR>"},
-    }
-  },
-  --add dev icons TODO: remove boxes around icons?
-  -- {
-  --   "nvim-tree/nvim-web-devicons",
-  --  -- dependencies = {
-  --  --   {"preservim/nerdtree"},
-  --  --   {"vim-airline/vim-airline"}
-  --  -- },
-  --   --priority = 1, -- lowest
-  --   config = true
-  -- },
-  {
-    "ryanoasis/vim-devicons",
-    dependencies = {
-      {"preservim/nerdtree"},
-      {"vim-airline/vim-airline"}
-    },
-    priority = 1, -- lowest
-  },
-  {
-    "tiagofumo/vim-nerdtree-syntax-highlight",
-    init = function()
-      vim.g.WebDevIconsDisableDefaultFolderSymbolColorFromNERDTreeDir = 1  
-      vim.g.WebDevIconsDisableDefaultFileSymbolColorFromNERDTreeFile = 1
-    end
-  }
-
+	{
+		"preservim/nerdtree",
+		keys = {
+			{ "<C-n>", "<cmd>NERDTree<CR>" },
+			{ "<C-t>", "<cmd>NERDTreeToggle<CR>" },
+			{ "<C-f>", "<cmd>NERDTreeFind<CR>" },
+		},
+	},
+	--add dev icons TODO: remove boxes around icons?
+	{
+		"ryanoasis/vim-devicons",
+		dependencies = {
+			{ "preservim/nerdtree" },
+			{ "vim-airline/vim-airline" },
+		},
+		priority = 1, -- lowest
+	},
+	{
+		"tiagofumo/vim-nerdtree-syntax-highlight",
+		init = function()
+			vim.g.WebDevIconsDisableDefaultFolderSymbolColorFromNERDTreeDir = 1
+			vim.g.WebDevIconsDisableDefaultFileSymbolColorFromNERDTreeFile = 1
+		end,
+	},
+	-- {
+	-- 	"nvim-tree/nvim-web-devicons",
+	-- 	dependencies = {
+	-- 		{ "preservim/nerdtree" },
+	-- 		{ "vim-airline/vim-airline" },
+	-- 	},
+	-- 	priority = 1, -- lowest
+	-- 	config = true,
+	-- },
 }
--- nvim_lua-lazy/nvim/lua/plugins/telescope.lua
+++ nvim_lua-lazy/nvim/lua/plugins/telescope.lua
@@ -1,23 +1,31 @@
 return {
-    'nvim-telescope/telescope.nvim',
-    tag = '0.1.5',
-    dependencies = { 'nvim-lua/plenary.nvim' },
-    keys={
-      { "<leader>ff", "<cmd>Telescope find_files<cr>", desc = "find files"},
-      { "<leader>fgh", "<cmd>Telescope find_files hidden=true<cr>", desc = "find hidden"},
-      { "<leader>fg", "<cmd>Telescope live_grep<cr>", desc = "grep"},
-      { "<leader>fb", "<cmd>Telescope buffers<cr>"},
-      { "<leader>fh", "<cmd>Telescope help_tags<cr>"},
-      { "<leader><C-p>", "<cmd>Telescope git_files<cr>"}
-    },
-    opts = {
-      defaults = {
-        path_display = { "truncate" },
-        truncate = 3
-      }
-    }
+	{
+		"nvim-telescope/telescope.nvim",
+		tag = "0.1.5",
+		dependencies = { "nvim-lua/plenary.nvim" },
+		keys = {
+			{ "<leader>ff", "<cmd>Telescope find_files<cr>", desc = "find files" },
+			{ "<leader>fgh", "<cmd>Telescope find_files hidden=true<cr>", desc = "find hidden" },
+			{ "<leader>fg", "<cmd>Telescope live_grep<cr>", desc = "grep" },
+			{ "<leader>fb", "<cmd>Telescope buffers<cr>" },
+			{ "<leader>fh", "<cmd>Telescope help_tags<cr>" },
+			{ "<leader><C-p>", "<cmd>Telescope git_files<cr>" },
+		},
+		opts = {
+			defaults = {
+				path_display = { "truncate" },
+				truncate = 3,
+			},
+		},
+	},
+	{
+		"nvim-telescope/telescope-fzf-native.nvim",
+		build = "cmake -S. -Bbuild -DCMAKE_BUILD_TYPE=Release && cmake --build build --config Release && cmake --install build --prefix build",
+		config = function()
+			require("telescope").load_extension("fzf")
+		end,
+	},
 }
-
 
 --    keys = {
 --      {
