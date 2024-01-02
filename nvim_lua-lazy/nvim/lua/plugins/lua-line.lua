d78fa5df17cb756eed9c70e78b301fc1d9c20d68
Noah Harris <ncharris93@gmail.com>
2024-01-02 11:45:47 -0500

    fix: use global lualine

--- nvim_lua-lazy/nvim/lua/plugins/lua-line.lua
+++ nvim_lua-lazy/nvim/lua/plugins/lua-line.lua
@@ -1,7 +1,13 @@
 return {
-    {
-        'nvim-lualine/lualine.nvim',
-        dependencies = { 'nvim-tree/nvim-web-devicons' },
-        config = true
-    }
+	{
+		"nvim-lualine/lualine.nvim",
+		dependencies = { "nvim-tree/nvim-web-devicons" },
+		config = function()
+			require("lualine").setup({
+				options = {
+					globalstatus = true,
+				},
+			})
+		end,
+	},
 }
