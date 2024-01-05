bb54add6f72b9687c701c91aaa3867be559b085d
Noah Harris <ncharris93@gmail.com>
2024-01-05 15:53:57 -0500

    feat: add sticky headers

--- nvim_lua-lazy/nvim/lua/plugins/trouble.lua
+++ nvim_lua-lazy/nvim/lua/plugins/trouble.lua
@@ -1,9 +1,14 @@
 return {
- "folke/trouble.nvim",
- dependencies = { "nvim-tree/nvim-web-devicons" },
- config = true,
- keys = {
-        {"<leader>xx",function() require("trouble").toggle() end}
-    }
-
+	"folke/trouble.nvim",
+	dependencies = { "nvim-tree/nvim-web-devicons" },
+	config = true,
+	keys = {
+		{
+			"<leader>xx",
+			function()
+				require("trouble").toggle()
+			end,
+			desc = "toggle issue buffer",
+		},
+	},
 }
