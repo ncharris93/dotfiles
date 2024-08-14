2aa0a4257e08e1ad55e3d91bf1ae15db123c93b6
Noah Harris <ncharris93@gmail.com>
2024-08-14 09:26:02 -0400

    wip - wezterm init config

---
+++ wezterm/wezterm.lua
@@ -0,0 +1,130 @@
+-- Import the wezterm module
+local wezterm = require("wezterm")
+local appearance = require("appearance")
+-- Creates a config object which we will be adding our config to
+local config = wezterm.config_builder()
+
+-- + (This is where our config will go)
+if appearance.is_dark() then
+	config.color_scheme = "Tokyo Night"
+else
+	config.color_scheme = "Tokyo Night Day"
+end
+
+config.font = wezterm.font({ family = "FiraCode Nerd Font Mono" })
+config.font_size = 14
+
+config.window_background_opacity = 0.9
+config.macos_window_background_blur = 30
+
+config.window_decorations = "INTEGRATED_BUTTONS"
+
+local function resize_pane(key, direction)
+	return {
+		key = key,
+		action = wezterm.action.AdjustPaneSize({ direction, 3 }),
+	}
+end
+
+config.keys = {
+	{
+		-- When the left arrow is pressed
+		key = "LeftArrow",
+		-- With the "Option" key modifier held down
+		mods = "OPT",
+		-- Perform this action, in this case - sending ESC + B
+		-- to the terminal
+		action = wezterm.action.SendString("\x1bb"),
+	},
+	{
+		key = "RightArrow",
+		mods = "OPT",
+		action = wezterm.action.SendString("\x1bf"),
+	},
+	{
+		-- open wezterm config in nvim when cmd + , is pressed
+		key = ",",
+		mods = "SUPER",
+		action = wezterm.action.SpawnCommandInNewTab({
+			cwd = wezterm.home_dir,
+			args = { "nvim", wezterm.config_file },
+		}),
+	},
+	{
+		-- I'm used to tmux bindings, so am using the quotes (") key to
+		-- split horizontally, and the percent (%) key to split vertically.
+		key = '"',
+		-- Note that instead of a key modifier mapped to a key on your keyboard
+		-- like CTRL or ALT, we can use the LEADER modifier instead.
+		-- This means that this binding will be invoked when you press the leader
+		-- (CTRL + A), quickly followed by quotes (").
+		mods = "LEADER",
+		action = wezterm.action.SplitHorizontal({ domain = "CurrentPaneDomain" }),
+	},
+	{
+		key = "%",
+		mods = "LEADER",
+		action = wezterm.action.SplitVertical({ domain = "CurrentPaneDomain" }),
+	},
+	{
+		key = "x",
+		mods = "LEADER",
+		action = wezterm.action.CloseCurrentPane({ confirm = false }),
+	},
+	{
+		-- When we push LEADER + R...
+		key = "r",
+		mods = "LEADER",
+		-- Activate the `resize_panes` keytable
+		action = wezterm.action.ActivateKeyTable({
+			name = "resize_panes",
+			-- Ensures the keytable stays active after it handles its
+			-- first keypress.
+			one_shot = false,
+			-- Deactivate the keytable after a timeout.
+			timeout_milliseconds = 1000,
+		}),
+	},
+}
+
+config.key_tables = {
+	resize_panes = {
+		resize_pane("j", "Down"),
+		resize_pane("k", "Up"),
+		resize_pane("h", "Left"),
+		resize_pane("l", "Right"),
+	},
+}
+
+config.set_environment_variables = {
+	PATH = "/opt/homebrew/bin:" .. os.getenv("PATH"),
+}
+
+wezterm.on("update-status", function(window)
+	-- Grab the utf8 character for the "powerline" left facing
+	-- solid arrow.
+	local SOLID_LEFT_ARROW = utf8.char(0xe0b2)
+
+	-- Grab the current window's configuration, and from it the
+	-- palette (this is the combination of your chosen colour scheme
+	-- including any overrides).
+	local color_scheme = window:effective_config().resolved_palette
+	local bg = color_scheme.background
+	local fg = color_scheme.foreground
+
+	window:set_right_status(wezterm.format({
+		-- First, we draw the arrow...
+		{ Background = { Color = "none" } },
+		{ Foreground = { Color = bg } },
+		{ Text = SOLID_LEFT_ARROW },
+		-- Then we draw our text
+		{ Background = { Color = bg } },
+		{ Foreground = { Color = fg } },
+		{ Text = " " .. wezterm.hostname() .. " " },
+	}))
+end)
+
+config.leader = { key = "a", mods = "CTRL", timeout_milliseconds = 1000 }
+
+-- Returns our config to be evaluated. We must always do this at the bottom of this file
+return config
