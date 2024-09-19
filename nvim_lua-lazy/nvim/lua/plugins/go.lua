54c342c4b32a57334b8322aaca3c40d612ecc778
Noah Harris <ncharris93@gmail.com>
2024-09-19 15:34:56 -0400

    fix: go format on save

--- nvim_lua-lazy/nvim/lua/plugins/go.lua
+++ nvim_lua-lazy/nvim/lua/plugins/go.lua
@@ -1,12 +1,22 @@
 return {
 	"ray-x/go.nvim",
 	dependencies = { -- optional packages
-		"ray-x/guihua.lua",
+		"ray-x/guihua.lua", -- for floating windows
 		"neovim/nvim-lspconfig",
 		"nvim-treesitter/nvim-treesitter",
 	},
 	config = function()
 		require("go").setup()
+
+		-- format on save
+		local format_sync_grp = vim.api.nvim_create_augroup("GoFormat", {})
+		vim.api.nvim_create_autocmd("BufWritePre", {
+			pattern = "*.go",
+			callback = function()
+				require("go.format").gofmt()
+			end,
+			group = format_sync_grp,
+		})
 	end,
 	event = { "CmdlineEnter" },
 	ft = { "go", "gomod" },
