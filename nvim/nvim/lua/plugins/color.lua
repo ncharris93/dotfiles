5c39037dd30cabfe1fa1275677e26bcfa47bde88
Noah Harris <ncharris93@gmail.com>
2025-02-03 11:29:40 -0500

    feat: colorizer to show hex color previews

--- nvim/nvim/lua/plugins/color.lua
+++ nvim/nvim/lua/plugins/color.lua
@@ -1,24 +1,31 @@
 return {
 	{
-		{
-			"folke/tokyonight.nvim",
-			lazy = false,
-			priority = 1000,
-			config = function()
-				require("tokyonight").setup({
-					transparent = true,
-					day_brightness = 0.5,
-				})
-				-- Apply the tokyonight theme settings
-				vim.g.tokyonight_transparent = true
-				vim.cmd([[colorscheme tokyonight]])
+		"folke/tokyonight.nvim",
+		lazy = false,
+		priority = 1000,
+		config = function()
+			require("tokyonight").setup({
+				transparent = true,
+				day_brightness = 0.5,
+			})
+			-- Apply the tokyonight theme settings
+			vim.g.tokyonight_transparent = true
+			vim.cmd([[colorscheme tokyonight]])
 
-				-- Custom highlight settings for line numbers
-				vim.cmd([[
+			-- Custom highlight settings for line numbers
+			vim.cmd([[
           highlight LineNr guifg=#7aa2f7
           highlight CursorLineNr guifg=#7aa2f7
         ]])
-			end,
-		},
+		end,
+	},
+	{
+		"norcalli/nvim-colorizer.lua",
+		event = "BufReadPost",
+		config = function()
+			require("colorizer").setup({
+				"*", -- Enable colorizer for all file types
+			})
+		end,
 	},
 }
