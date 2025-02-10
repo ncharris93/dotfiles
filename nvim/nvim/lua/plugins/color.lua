9d6b97d3918ae2642f3157f6262ea7d4cfe3df4c
Noah Harris <ncharris93@gmail.com>
2025-02-10 14:04:22 -0500

    chore: make line numbers easier to read

--- nvim/nvim/lua/plugins/color.lua
+++ nvim/nvim/lua/plugins/color.lua
@@ -7,16 +7,26 @@ return {
 			require("tokyonight").setup({
 				transparent = true,
 				day_brightness = 0.5,
+				on_highlights = function(hl)
+					hl.LineNrAbove = {
+						fg = "#6ab8ff",
+					}
+					hl.LineNrBelow = {
+						fg = "#ff6188",
+					}
+				end,
 			})
 			-- Apply the tokyonight theme settings
 			vim.g.tokyonight_transparent = true
 			vim.cmd([[colorscheme tokyonight]])
 
 			-- Custom highlight settings for line numbers
+			-- highlight CursorLineNr guifg=#a9b1d6
 			vim.cmd([[
-          highlight LineNr guifg=#7aa2f7
-          highlight CursorLineNr guifg=#7aa2f7
-        ]])
+				highlight LineNr guifg=#5eacd3
+				highlight CursorLineNr guifg=#5eacd3
+				highlight Comment guifg=#a9b1d6
+			]])
 		end,
 	},
 	{
