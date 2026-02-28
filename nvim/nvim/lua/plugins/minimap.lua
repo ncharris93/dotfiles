3953082889c8c687d58f42c3c8cd80ad3cccf367
Noah Harris <ncharris93@gmail.com>
2026-02-28 09:02:54 -0500

    feat: add right gutters via satellite.nvim

--- nvim/nvim/lua/plugins/minimap.lua
+++ nvim/nvim/lua/plugins/minimap.lua
@@ -1,21 +1,10 @@
 return {
-  -- {
-  --   "wfxr/minimap.vim", 
-  --   init = function()
-  --      vim.g.minimap_width = 5
-  --      vim.g.minimap_auto_start = 1
-  --      vim.g.minimap_auto_start_win_enter = 1
-  --   end,
-  -- },
-  --  can't be used with nvim-scrollview
-  -- {
-  --   "dsummersl/nvim-sluice", 
-  --    config = true
-  -- }, 
   {
-    "dstein64/nvim-scrollview",
-    config = true
-  }
+    "lewis6991/satellite.nvim",
+    event = "BufReadPost",
+    opts = {
+      winblend = 40,
+      width = 2,
+    },
+  },
 }
-
--- { "lewis6991/satellite.nvim" } -- requires neovim 10
